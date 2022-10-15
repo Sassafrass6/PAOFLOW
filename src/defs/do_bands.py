56267650d0d2458eb5aaf816df294afd712586f3
Frank Cerasoli <ftcerasoli@ucdavis.edu>
2022-10-15 16:25:55 -0700

    Implemented ACBN0 automated routine. Example in examples/acbn0-exampeles

--- src/defs/do_bands.py
+++ src/defs/do_bands.py
@@ -1,20 +1,20 @@
 #
 # PAOFLOW
 #
-# Utility to construct and operate on Hamiltonians from the Projections of DFT wfc on Atomic Orbital bases (PAO)
-#
-# Copyright (C) 2016-2018 ERMES group (http://ermes.unt.edu, mbn@unt.edu)
+# Copyright 2016-2022 - Marco BUONGIORNO NARDELLI (mbn@unt.edu)
 #
 # Reference:
-# M. Buongiorno Nardelli, F. T. Cerasoli, M. Costa, S Curtarolo,R. De Gennaro, M. Fornari, L. Liyanage, A. Supka and H. Wang,
-# PAOFLOW: A utility to construct and operate on ab initio Hamiltonians from the Projections of electronic wavefunctions on
+#
+#F.T. Cerasoli, A.R. Supka, A. Jayaraj, I. Siloi, M. Costa, J. Slawinska, S. Curtarolo, M. Fornari, D. Ceresoli, and M. Buongiorno Nardelli, Advanced modeling of materials with PAOFLOW 2.0: New features and software design, Comp. Mat. Sci. 200, 110828 (2021).
+#
+# M. Buongiorno Nardelli, F. T. Cerasoli, M. Costa, S Curtarolo,R. De Gennaro, M. Fornari, L. Liyanage, A. Supka and H. Wang, 
+# PAOFLOW: A utility to construct and operate on ab initio Hamiltonians from the Projections of electronic wavefunctions on 
 # Atomic Orbital bases, including characterization of topological materials, Comp. Mat. Sci. vol. 143, 462 (2018).
 #
 # This file is distributed under the terms of the
 # GNU General Public License. See the file `License'
 # in the root directory of the present distribution,
 # or http://www.gnu.org/copyleft/gpl.txt .
-#
 
 import numpy as np
 from scipy import linalg as spl
@@ -56,9 +56,8 @@ def band_loop_H ( data_controller, kq_aux ):
   Haux = np.zeros((nawf,nawf,nksize,nspin), dtype=complex, order="C")
 
   for ispin in range(nspin):
-    Haux[:,:,:,ispin] = np.tensordot(HRs[:,:,:,ispin], kdot, axes=([2],[0]))
+    Haux[...,ispin] = np.tensordot(HRs[...,ispin], kdot, axes=([2],[0]))
 
-  kdot  = None
   return Haux
 
 
--- src/defs/do_find_Weyl.py
+++ src/defs/do_find_Weyl.py
@@ -1,30 +1,20 @@
 #
-# PAOpy
+# PAOFLOW
 #
-# Utility to construct and operate on Hamliltonians from the Projections of DFT wfc on Atomic Orbital bases (PAO)
+# Copyright 2016-2022 - Marco BUONGIORNO NARDELLI (mbn@unt.edu)
 #
-# Copyright (C) 2016 ERMES group (http://ermes.unt.edu)
+# Reference:
+#
+#F.T. Cerasoli, A.R. Supka, A. Jayaraj, I. Siloi, M. Costa, J. Slawinska, S. Curtarolo, M. Fornari, D. Ceresoli, and M. Buongiorno Nardelli, Advanced modeling of materials with PAOFLOW 2.0: New features and software design, Comp. Mat. Sci. 200, 110828 (2021).
+#
+# M. Buongiorno Nardelli, F. T. Cerasoli, M. Costa, S Curtarolo,R. De Gennaro, M. Fornari, L. Liyanage, A. Supka and H. Wang, 
+# PAOFLOW: A utility to construct and operate on ab initio Hamiltonians from the Projections of electronic wavefunctions on 
+# Atomic Orbital bases, including characterization of topological materials, Comp. Mat. Sci. vol. 143, 462 (2018).
+#
 # This file is distributed under the terms of the
 # GNU General Public License. See the file `License'
 # in the root directory of the present distribution,
 # or http://www.gnu.org/copyleft/gpl.txt .
-#
-#
-# References:
-# Luis A. Agapito, Andrea Ferretti, Arrigo Calzolari, Stefano Curtarolo and Marco Buongiorno Nardelli,
-# Effective and accurate representation of extended Bloch states on finite Hilbert spaces, Phys. Rev. B 88, 165127 (2013).
-#
-# Luis A. Agapito, Sohrab Ismail-Beigi, Stefano Curtarolo, Marco Fornari and Marco Buongiorno Nardelli,
-# Accurate Tight-Binding Hamiltonian Matrices from Ab-Initio Calculations: Minimal Basis Sets, Phys. Rev. B 93, 035104 (2016).
-#
-# Luis A. Agapito, Marco Fornari, Davide Ceresoli, Andrea Ferretti, Stefano Curtarolo and Marco Buongiorno Nardelli,
-# Accurate Tight-Binding Hamiltonians for 2D and Layered Materials, Phys. Rev. B 93, 125137 (2016).
-#
-# Pino D'Amico, Luis gapito, Alessandra Catellani, Alice Ruini, Stefano Curtarolo, Marco Fornari, Marco Buongiorno Nardelli, 
-# and Arrigo Calzolari, Accurate ab initio tight-binding Hamiltonians: Effective tools for electronic transport and 
-# optical spectroscopy from first principles, Phys. Rev. B 94 165166 (2016).
-# 
-
 
 import scipy.optimize as so
 #import matplotlib.pyplot as plt
@@ -117,6 +107,7 @@ def get_search_grid ( nk1, nk2, nk3, snk1_range=[-0.5,0.5], snk2_range=[-0.5,0.5
   return np.array(np.meshgrid(nk1_arr,nk2_arr,nk3_arr,indexing='ij')).T.reshape(-1,3)
 
 def find_weyl ( data_controller, test_rad, search_grid ):
+  import os
 
   arry,attr = data_controller.data_dicts()
 
@@ -151,7 +142,6 @@ def find_weyl ( data_controller, test_rad, search_grid ):
     try:
       import z2pack
       import tbmodels      
-      import os
       model = tbmodels.Model.from_wannier_files(hr_file=os.path.join(attr['opath'],'z2pack_hamiltonian.dat'))
       system = z2pack.tb.System(model,bands=nelec)
 
